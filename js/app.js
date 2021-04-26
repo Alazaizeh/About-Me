320e52a839e2a6e559c9c94909975fb7536b4746
Omar Alazaizeh <omx302@gmail.com>
2021-04-26 14:01:16 +0300

    edit js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 'use strict'
+
+let userName = prompt("Please Enter Your Name ?");
+alert(`Welcome ${userName} to my site :) `);
+
+let coffee = prompt("Do you think I love coffee ?").toUpperCase();
+if (coffee == "Y" || coffee == "YES") {
+
+} else {
+
+}
+let football = prompt("Do you think I watch football ?").toUpperCase();
+if (football == "N" || football == "NO") {
+
+} else {
+
+}
+let movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
+if (movie == "Y" || movie == "YES") {
+
+} else {
+
+}
+let degree = prompt("Do you think I have master degree ?").toUpperCase();
+if (degree == "N" || degree == "NO") {
+
+} else {
+
+}
+let weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
+if (weather == "N" || weather == "NO") {
+
+} else {
+
+}
+
+alert(`Nice to meet you ${userName} :) `);
