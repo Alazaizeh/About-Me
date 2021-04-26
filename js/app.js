1c4283e42e3ccb01a0bda4587945a334e2b8355b
Omar Alazaizeh <omx302@gmail.com>
2021-04-26 14:45:32 +0300

    edit js 2

--- js/app.js
+++ js/app.js
@@ -4,33 +4,67 @@ let userName = prompt("Please Enter Your Name ?");
 alert(`Welcome ${userName} to my site :) `);
 
 let coffee = prompt("Do you think I love coffee ?").toUpperCase();
+while (coffee != 'Y' && coffee != 'N' && coffee != 'YES' && coffee != 'NO') {
+    coffee = prompt("Do you think I love coffee ?").toUpperCase();
+}
 if (coffee == "Y" || coffee == "YES") {
+    alert("Great, CORRECT Answer");
 
-} else {
+} else if (coffee == "N" || coffee == "NO") {
+
+    alert("WRONG Answer !!");
 
 }
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 let football = prompt("Do you think I watch football ?").toUpperCase();
+
+while (football != 'Y' && football != 'N' && football != 'YES' && football != 'NO') {
+    football = prompt("Do you think I watch football ?").toUpperCase();
+}
 if (football == "N" || football == "NO") {
+    alert("Great, CORRECT Answer");
 
-} else {
+} else if (football == "Y" || football == "YES") {
+    alert("WRONG Answer !!");
 
 }
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 let movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
+while (movie != 'Y' && movie != 'N' && movie != 'YES' && movie != 'NO') {
+    movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
+}
 if (movie == "Y" || movie == "YES") {
+    alert("Great, CORRECT Answer");
 
-} else {
+} else if (movie == "N" || movie == "NO") {
+    alert("WRONG Answer !!");
 
 }
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 let degree = prompt("Do you think I have master degree ?").toUpperCase();
+while (degree != 'Y' && degree != 'N' && degree != 'YES' && degree != 'NO') {
+    degree = prompt("Do you think I have master degree ?").toUpperCase();
+}
 if (degree == "N" || degree == "NO") {
+    alert("Great, CORRECT Answer");
 
-} else {
+} else if (degree == "Y" || degree == "YES") {
+    alert("WRONG Answer !!");
 
 }
-let weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
-if (weather == "N" || weather == "NO") {
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-} else {
+let weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
+while (weather != 'Y' && weather != 'N' && weather != 'YES' && weather != 'NO') {
+    weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
+}
+if (weather == "N" || weather == "NO") {
+    alert("Great, CORRECT Answer");
+
+} else if (weather == "Y" || weather == "YES") {
+    alert("WRONG Answer !!");
 
 }
 
