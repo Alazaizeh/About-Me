dec3a2bddc2eece45736b992c6ab2b5c8b6892d7
Omar Alazaizeh <omx302@gmail.com>
2021-04-27 03:02:36 +0300

    update style

--- js/app.js
+++ js/app.js
@@ -1,95 +1,95 @@
-// 'use strict'
+'use strict'
 
-// let userName = prompt("Please Enter Your Name ?");
-// alert(`Welcome ${userName} to my site :) `);
-// let score = 0;
+let userName = prompt("Please Enter Your Name ?");
+alert(`Welcome ${userName} to my site :) `);
+let score = 0;
 
-// let coffee = prompt("Do you think I love coffee ?").toUpperCase();
-// while (coffee != 'Y' && coffee != 'N' && coffee != 'YES' && coffee != 'NO') {
-//     coffee = prompt("Do you think I love coffee ?").toUpperCase();
-// }
-// if (coffee == "Y" || coffee == "YES") {
-//     // console.log("Correct");
-//     score++;
-//     alert(`Great, CORRECT Answer ${score} of 1`);
-
-
-// } else if (coffee == "N" || coffee == "NO") {
-//     // console.log("Wrong");
-//     alert(`WRONG Answer !! ${score} of 1`);
-
-// }
-
-// let football = prompt("Do you think I watch football ?").toUpperCase();
-
-// while (football != 'Y' && football != 'N' && football != 'YES' && football != 'NO') {
-//     football = prompt("Do you think I watch football ?").toUpperCase();
-// }
-// if (football == "N" || football == "NO") {
-//     // console.log("Correct");
-
-//     score++;
-//     alert(`Great, CORRECT Answer ${score} of 2`);
-
-// } else if (football == "Y" || football == "YES") {
-//     // console.log("Wrong");
-//     alert(`WRONG Answer !! ${score} of 2`);
+let coffee = prompt("Do you think I love coffee ?").toUpperCase();
+while (coffee != 'Y' && coffee != 'N' && coffee != 'YES' && coffee != 'NO') {
+    coffee = prompt("Do you think I love coffee ?").toUpperCase();
+}
+if (coffee == "Y" || coffee == "YES") {
+    // console.log("Correct");
+    score++;
+    alert(`Great, CORRECT Answer ${score} of 1`);
 
 
-// }
+} else if (coffee == "N" || coffee == "NO") {
+    // console.log("Wrong");
+    alert(`WRONG Answer !! ${score} of 1`);
+
+}
+
+let football = prompt("Do you think I watch football ?").toUpperCase();
+
+while (football != 'Y' && football != 'N' && football != 'YES' && football != 'NO') {
+    football = prompt("Do you think I watch football ?").toUpperCase();
+}
+if (football == "N" || football == "NO") {
+    // console.log("Correct");
+
+    score++;
+    alert(`Great, CORRECT Answer ${score} of 2`);
+
+} else if (football == "Y" || football == "YES") {
+    // console.log("Wrong");
+    alert(`WRONG Answer !! ${score} of 2`);
 
 
-// let movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
-// while (movie != 'Y' && movie != 'N' && movie != 'YES' && movie != 'NO') {
-//     movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
-// }
-// if (movie == "Y" || movie == "YES") {
-//     // console.log("Correct");
-
-//     score++;
-//     alert(`Great, CORRECT Answer ${score} of 3`);
-
-// } else if (movie == "N" || movie == "NO") {
-//     // console.log("Wrong");
-//     alert(`WRONG Answer !! ${score} of 3`);
+}
 
 
-// }
+let movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
+while (movie != 'Y' && movie != 'N' && movie != 'YES' && movie != 'NO') {
+    movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
+}
+if (movie == "Y" || movie == "YES") {
+    // console.log("Correct");
+
+    score++;
+    alert(`Great, CORRECT Answer ${score} of 3`);
+
+} else if (movie == "N" || movie == "NO") {
+    // console.log("Wrong");
+    alert(`WRONG Answer !! ${score} of 3`);
 
 
-// let degree = prompt("Do you think I have master degree ?").toUpperCase();
-// while (degree != 'Y' && degree != 'N' && degree != 'YES' && degree != 'NO') {
-//     degree = prompt("Do you think I have master degree ?").toUpperCase();
-// }
-// if (degree == "N" || degree == "NO") {
-//     // console.log("Correct");
-
-//     score++;
-//     alert(`Great, CORRECT Answer ${score} of 4`);
-
-// } else if (degree == "Y" || degree == "YES") {
-//     // console.log("Wrong");
-//     alert(`WRONG Answer !! ${score} of 4`);
+}
 
 
-// }
+let degree = prompt("Do you think I have master degree ?").toUpperCase();
+while (degree != 'Y' && degree != 'N' && degree != 'YES' && degree != 'NO') {
+    degree = prompt("Do you think I have master degree ?").toUpperCase();
+}
+if (degree == "N" || degree == "NO") {
+    // console.log("Correct");
+
+    score++;
+    alert(`Great, CORRECT Answer ${score} of 4`);
+
+} else if (degree == "Y" || degree == "YES") {
+    // console.log("Wrong");
+    alert(`WRONG Answer !! ${score} of 4`);
 
 
-// let weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
-// while (weather != 'Y' && weather != 'N' && weather != 'YES' && weather != 'NO') {
-//     weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
-// }
-// if (weather == "N" || weather == "NO") {
-//     // console.log("Correct");
-
-//     score++;
-//     alert(`Great, CORRECT Answer ${score} of 5`);
-
-// } else if (weather == "Y" || weather == "YES") {
-//     // console.log("Wrong");
-//     alert(`WRONG Answer !! ${score} of 5`);
+}
 
 
-// }
+let weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
+while (weather != 'Y' && weather != 'N' && weather != 'YES' && weather != 'NO') {
+    weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
+}
+if (weather == "N" || weather == "NO") {
+    // console.log("Correct");
 
-// alert(`Nice to meet you ${userName} :) `);
+    score++;
+    alert(`Great, CORRECT Answer ${score} of 5`);
+
+} else if (weather == "Y" || weather == "YES") {
+    // console.log("Wrong");
+    alert(`WRONG Answer !! ${score} of 5`);
+
+
+}
+
+alert(`Nice to meet you ${userName} :) `);
