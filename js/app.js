320f12b8ee82fe152c9ff2c52492102d3271b2dd
Omar Alazaizeh <omx302@gmail.com>
2021-04-27 22:41:03 +0300

    lab 3 update

--- js/app.js
+++ js/app.js
@@ -1,95 +1,175 @@
-'use strict'
+"use strict";
 
-let userName = prompt("Please Enter Your Name ?");
-alert(`Welcome ${userName} to my site :) `);
 let score = 0;
+let questionCounter = 0;
+let attempts = 0;
 
-let coffee = prompt("Do you think I love coffee ?").toUpperCase();
-while (coffee != 'Y' && coffee != 'N' && coffee != 'YES' && coffee != 'NO') {
-    coffee = prompt("Do you think I love coffee ?").toUpperCase();
+let userName = null;
+while (userName == null || userName == "") {
+  userName = prompt("Please Enter Your Name ?");
+}
+console.log(userName);
+alert(`Welcome ${userName} to my site ^_^ `);
+
+let coffee = null;
+questionCounter++;
+while (coffee != "Y" && coffee != "N" && coffee != "YES" && coffee != "NO") {
+  coffee = prompt(`Q${questionCounter}: Do you think I love coffee ?`);
+  if (coffee != null) {
+    coffee = coffee.toUpperCase();
+  }
 }
 if (coffee == "Y" || coffee == "YES") {
-    // console.log("Correct");
-    score++;
-    alert(`Great, CORRECT Answer ${score} of 1`);
-
-
+  // console.log("Correct");
+  score++;
+  alert(`Great, CORRECT Answer `);
 } else if (coffee == "N" || coffee == "NO") {
-    // console.log("Wrong");
-    alert(`WRONG Answer !! ${score} of 1`);
-
+  // console.log("Wrong");
+  alert(`WRONG Answer !!`);
 }
 
-let football = prompt("Do you think I watch football ?").toUpperCase();
-
-while (football != 'Y' && football != 'N' && football != 'YES' && football != 'NO') {
-    football = prompt("Do you think I watch football ?").toUpperCase();
+let football = null;
+questionCounter++;
+while (
+  football != "Y" &&
+  football != "N" &&
+  football != "YES" &&
+  football != "NO"
+) {
+  football = prompt(`Q${questionCounter}: Do you think I watch football ?`);
+  if (football != null) {
+    football = football.toUpperCase();
+  }
 }
 if (football == "N" || football == "NO") {
-    // console.log("Correct");
+  // console.log("Correct");
 
-    score++;
-    alert(`Great, CORRECT Answer ${score} of 2`);
-
+  score++;
+  alert(`Great, CORRECT Answer `);
 } else if (football == "Y" || football == "YES") {
-    // console.log("Wrong");
-    alert(`WRONG Answer !! ${score} of 2`);
-
-
+  // console.log("Wrong");
+  alert(`WRONG Answer !!`);
 }
 
-
-let movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
-while (movie != 'Y' && movie != 'N' && movie != 'YES' && movie != 'NO') {
-    movie = prompt("Do you think I like action movie more than romance ?").toUpperCase();
+let movie = null;
+questionCounter++;
+while (movie != "Y" && movie != "N" && movie != "YES" && movie != "NO") {
+  movie = prompt(
+    `Q${questionCounter}: Do you think I like action movie more than romance ?`
+  );
+  if (movie != null) {
+    movie = movie.toUpperCase();
+  }
 }
 if (movie == "Y" || movie == "YES") {
-    // console.log("Correct");
+  // console.log("Correct");
 
-    score++;
-    alert(`Great, CORRECT Answer ${score} of 3`);
-
+  score++;
+  alert(`Great, CORRECT Answer `);
 } else if (movie == "N" || movie == "NO") {
-    // console.log("Wrong");
-    alert(`WRONG Answer !! ${score} of 3`);
-
-
+  // console.log("Wrong");
+  alert(`WRONG Answer !! `);
 }
 
-
-let degree = prompt("Do you think I have master degree ?").toUpperCase();
-while (degree != 'Y' && degree != 'N' && degree != 'YES' && degree != 'NO') {
-    degree = prompt("Do you think I have master degree ?").toUpperCase();
+let degree = null;
+questionCounter++;
+while (degree != "Y" && degree != "N" && degree != "YES" && degree != "NO") {
+  degree = prompt(`Q${questionCounter}: Do you think I have master degree ?`);
+  if (degree != null) {
+    degree = degree.toUpperCase();
+  }
 }
 if (degree == "N" || degree == "NO") {
-    // console.log("Correct");
+  // console.log("Correct");
 
-    score++;
-    alert(`Great, CORRECT Answer ${score} of 4`);
-
+  score++;
+  alert(`Great, CORRECT Answer `);
 } else if (degree == "Y" || degree == "YES") {
-    // console.log("Wrong");
-    alert(`WRONG Answer !! ${score} of 4`);
-
-
+  // console.log("Wrong");
+  alert(`WRONG Answer !! `);
 }
 
-
-let weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
-while (weather != 'Y' && weather != 'N' && weather != 'YES' && weather != 'NO') {
-    weather = prompt("Do you think I prefer Summer more than Winter ?").toUpperCase();
+let weather = null;
+questionCounter++;
+while (
+  weather != "Y" &&
+  weather != "N" &&
+  weather != "YES" &&
+  weather != "NO"
+) {
+  weather = prompt(
+    `Q${questionCounter}: Do you think I prefer Summer more than Winter ?`
+  );
+  if (weather != null) {
+    weather = weather.toUpperCase();
+  }
 }
 if (weather == "N" || weather == "NO") {
-    // console.log("Correct");
+  // console.log("Correct");
 
-    score++;
-    alert(`Great, CORRECT Answer ${score} of 5`);
-
+  score++;
+  alert(`Great, CORRECT Answer`);
 } else if (weather == "Y" || weather == "YES") {
-    // console.log("Wrong");
-    alert(`WRONG Answer !! ${score} of 5`);
-
-
+  // console.log("Wrong");
+  alert(`WRONG Answer !!`);
 }
 
-alert(`Nice to meet you ${userName} :) `);
+let myAge = null;
+questionCounter++;
+for (let i = 1; i <= 4; i++) {
+  myAge = Number(
+    prompt(`Q${questionCounter}: Can you guess my age ? Enter a number  ?`)
+  );
+  console.log(typeof myAge);
+  console.log(myAge);
+  if (myAge == 23) {
+    // console.log("Correct");
+    alert(`Great, CORRECT Answer`);
+    score++;
+    break;
+  }
+  attempts++;
+  if (isNaN(myAge)) {
+    alert(`Enter a Valid number ! ${4 - attempts} attempts left`);
+  } else if (myAge > 23) {
+    alert(`Your guess is too high ! ${4 - attempts} attempts left`);
+  } else if (myAge < 23) {
+    alert(`Your guess is too low ! ${4 - attempts} attempts left`);
+  }
+}
+if (attempts >= 4) {
+  // console.log("Wrong");
+  alert(`WRONG Answer !! The Correct Number is 23`);
+}
+attempts = 0;
+
+let countries = ["MEXICO", "EGYPT", "IRELAND", "CANADA", "CHINA"];
+let country = null;
+questionCounter++;
+
+for (let i = 1; i <= 6; i++) {
+  country = prompt(
+    `Q${questionCounter}: Can you guess which country I want to visit ?`
+  );
+  if (country != null) {
+    country = country.toUpperCase();
+  }
+  if (countries.indexOf(country) != -1) {
+    // console.log("Correct");
+    score++;
+    alert(`Great, CORRECT Answer `);
+    break;
+  } else {
+    attempts++;
+    // console.log("Wrong");
+    alert(`WRONG Answer !! ${6 - attempts} attempts left`);
+  }
+}
+if (attempts >= 6) {
+  // console.log("Wrong");
+  alert(`WRONG Answer !! The Correct Ccountries: \n${countries.join("\n")}`);
+}
+
+alert(
+  `Well Played ${userName}, Your overall Score ${score} out of ${questionCounter}`
+);
